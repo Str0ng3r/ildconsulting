d70a6c6f2b6589584881ff4d1b7c44bf7879e2d4
Maks Stankevich <maksstankevich81@gmail.com>
2024-07-30 18:08:40 +0200

    page kingdom

---
+++ consult/pages/kingdom/index.vue
@@ -0,0 +1,114 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="container">
+		<div class="container_wrap cont_kingdom">
+			<h1 class="visa_title title_king">
+				Послуги всередині Сполученого Королівства
+			</h1>
+			<p class="first_desc">
+				- NINo (National Insurance Number) – це аналог української картки
+				платника податків.
+			</p>
+			<p class="sec_desc">
+				Допоможемо з подачею на отримання першого іншуренсу або з відновленням
+				втраченого. <br />
+				- Відкриття банківського рахунку
+			</p>
+			<p class="banks_desc">
+				Відкриємо банківські рахунки у таких банках: <br />
+				1)   NatWest <br />
+				2)   Bacrlays <br />
+				3)   Lloyds <br />
+				4)   HSBC <br />
+				5)   Santander <br />
+				6)   Monzo <br />
+				7)   Revolut
+			</p>
+			<p class="main_desc">
+				- Оформлення реєстрації UTR (Unique Taxpayer Reference) – це аналог
+				українського Фізичної-Особи підприємця (ФОП). <br />
+				- Подача на учбове водійське посвідчення (provisional driving licence)
+				<br />
+				- Подання на universal credit <br />
+				- Подання на державні виплати на дітей (child benefits) <br />
+				- Реєстрація в поліклініці (якщо є потреба, то підемо з Вами в
+				поліклініку) <br />
+				- Подання на британські пенсійні виплати (pension credit)<br />
+				- Оскарження штрафів <br />
+				- Повернення податків від держави (лише по візі Tier 5 – seasonal
+				working visa) <br />
+				- Подача на отримання біометричної резидент картки (BRP card) <br />
+				- Написання резюме та розсилка у відповідні компанії <br />
+				- Реєстрація на отримання апостилю на британські документи <br />
+				- Виловлення слотів в українському консульстві <br />
+				- Пошук житла під оренду
+			</p>
+		</div>
+	</div>
+</template>
+<style lang="scss" scoped>
+.container {
+	width: 100%;
+	margin: 0 auto;
+	background: #f2f7f4;
+	min-height: 80rem;
+	padding-top: 10rem;
+}
+
+.cont_kingdom {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	max-width: 97rem;
+	padding-bottom: 10rem;
+}
+.first_desc {
+	width: 100%;
+	color: #1d1d1d;
+	font-family: "Roboto";
+	margin-bottom: 8rem;
+	font-size: 1.6rem;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 3.2rem; /* 200% */
+}
+.main_desc {
+	width: 100%;
+	color: #1d1d1d;
+	font-family: "Roboto";
+	font-size: 1.6rem;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 3.2rem; /* 200% */
+}
+.sec_desc {
+	width: 100%;
+	color: #1d1d1d;
+	font-family: "Roboto";
+	margin-bottom: 8rem;
+	font-size: 1.6rem;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 3.2rem; /* 200% */
+}
+.banks_desc {
+	width: 100%;
+	color: #1d1d1d;
+	font-family: "Roboto";
+	margin-bottom: 8rem;
+	font-size: 1.6rem;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 3.2rem; /* 200% */
+}
+.title_king {
+	margin-bottom: 3.5rem;
+	color: #151515;
+	font-size: 3.2rem;
+	font-style: normal;
+	font-weight: 700;
+	line-height: 4rem;
+}
+</style>
